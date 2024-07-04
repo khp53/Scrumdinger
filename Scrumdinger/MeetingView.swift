2184536e4a4a7adbff416e0239c058294f51d4b4
KHP53 <khp4121@gmail.com>
2024-07-04 21:08:20 -0230

    added root app file and meeting view

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Karimul Hasan on 2024-07-01.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading, content: {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                })
+                Spacer()
+                VStack(alignment: .trailing, content: {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                })
+            }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle().strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
